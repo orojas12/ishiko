c7b5c492d7a3544795653bee45c2ea1611e5660c
Oscar Rojas <orojas12@outlook.com>
2023-05-29 13:40:07 -0600

    project files

---
+++ ishiko-server/src/test/java/app/ishiko/ishikoserver/IshikoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ishiko.ishikoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshikoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
