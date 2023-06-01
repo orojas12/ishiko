da7346e9c34eb11126ffa2a7f654ed24a88cb506
Oscar Rojas <orojas12@outlook.com>
2023-06-01 15:38:26 -0600

    protect authenticated routes

--- ishiko-client/src/pages/_app.tsx
+++ ishiko-client/src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import type { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import AppProvider from "@/providers/app";
+import { useRouter } from "next/router";
+import { useGetUserQuery } from "@/services";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,5 +17,45 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <AppProvider>
+      <ProtectedRoutes publicRoutes={["/", "/login"]}>
+        {getLayout(<Component {...pageProps} />)}
+      </ProtectedRoutes>
+    </AppProvider>
+  );
+}
+
+interface ProtectedRoutesProps {
+  children?: React.ReactNode;
+  publicRoutes?: string[];
+}
+
+function ProtectedRoutes({ children, publicRoutes }: ProtectedRoutesProps) {
+  const router = useRouter();
+  const { data: user } = useGetUserQuery();
+
+  const isPublic = publicRoutes?.includes(router.asPath);
+
+  useEffect(() => {
+    if (!user) {
+      if (isPublic) {
+        return;
+      } else {
+        router.replace("/login");
+      }
+    }
+  }, [user, isPublic]);
+
+  useEffect(() => {
+    if (user) {
+      router.asPath === "/login" && router.replace("/");
+    }
+  }, [user, router.asPath]);
+
+  if (!user && !isPublic) {
+    return <h1>LOADING APP</h1>;
+  } else {
+    return <>{children}</>;
+  }
 }
