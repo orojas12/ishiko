1d389dab4e044ac1be0c8e5127bda6a96670d4a3
Oscar Rojas <orojas12@outlook.com>
2023-06-21 18:33:09 -0600

    integrate createProject mutation and add tests

--- ishiko-client/src/modules/projects/components/CreateProject.tsx
+++ ishiko-client/src/modules/projects/components/CreateProject.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import { Box, Button, Dialog, DialogTitle, TextField } from "@mui/material";
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { useCreateProjectMutation } from "@/services";
+import { setCurrentProject } from "../stores/projects";
 
 export default function CreateProject() {
+  const dispatch = useAppDispatch();
+  const [createProject, result] = useCreateProjectMutation();
   const [open, setOpen] = useState(false);
+  const [projectTitle, setProjectTitle] = useState("");
+
+  useEffect(() => {
+    if (result.isSuccess) {
+      dispatch(setCurrentProject(result.data));
+    }
+  }, [result.data]);
 
   function handleClose() {
     setOpen(false);
@@ -15,17 +27,32 @@ export default function CreateProject() {
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <Box
+          component="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            createProject({ title: projectTitle });
+            setProjectTitle("");
+            setOpen(false);
+          }}
+          name="createProjectForm"
           sx={{ display: "flex", flexDirection: "column", gap: 4, padding: 4 }}
         >
           <DialogTitle sx={{ padding: 0 }}>Create new project</DialogTitle>
           <TextField
             id="createProjectTitle"
             label="Project Title"
+            onChange={(e) => setProjectTitle(e.target.value)}
             sx={{ width: 350 }}
+            required
+            value={projectTitle}
           />
           <Box sx={{ display: "flex", gap: 2, justifyContent: "flex-end" }}>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button variant="contained">Create</Button>
+            <Button onClick={handleClose} type="button">
+              Cancel
+            </Button>
+            <Button variant="contained" type="submit">
+              Create Project
+            </Button>
           </Box>
         </Box>
       </Dialog>
