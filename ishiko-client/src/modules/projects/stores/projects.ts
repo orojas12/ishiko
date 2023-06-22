94f8b5b0917933b19ac63a64db076fc1fd34b944
Oscar Rojas <orojas12@outlook.com>
2023-06-21 18:27:17 -0600

    change currentProject slice state

--- ishiko-client/src/modules/projects/stores/projects.ts
+++ ishiko-client/src/modules/projects/stores/projects.ts
@@ -1,15 +1,18 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Project } from "../types";
 
 export const currentProjectSlice = createSlice({
   name: "currentProject",
-  initialState: "" as string | undefined,
+  initialState: {} as Project,
   reducers: {
-    setCurrent: (state, action: PayloadAction<string | undefined>) => {
-      state = action.payload;
+    setCurrentProject: (state, action: PayloadAction<Project>) => {
+      state.id = action.payload.id;
+      state.title = action.payload.title;
+      state.owner = action.payload.owner;
     },
   },
 });
 
-export const { setCurrent } = currentProjectSlice.actions;
+export const { setCurrentProject } = currentProjectSlice.actions;
 
 export default currentProjectSlice.reducer;
