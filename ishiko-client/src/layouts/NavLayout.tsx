32b5b129544d83286b430a09947a4dcd0aef57da
Oscar Rojas <orojas12@outlook.com>
2023-06-21 18:35:21 -0600

    finish NavLayout component

--- ishiko-client/src/layouts/NavLayout.tsx
+++ ishiko-client/src/layouts/NavLayout.tsx
@@ -1,25 +1,71 @@
 import BaseLayout from "./BaseLayout";
-import { BrandLogo } from "@/components";
-import { ProjectControls } from "@/modules/projects";
+import { BrandLogo, Navigation } from "@/components";
+import { CreateProject, Projects } from "@/modules/projects";
 
 import {
+  AppBar,
   Box,
+  Button,
   Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
+  TextField,
+  Toolbar,
+  Typography,
 } from "@mui/material";
+import { useRouter } from "next/router";
 
 interface NavLayoutProps {
   children?: React.ReactNode;
 }
 
 export default function NavLayout(props: NavLayoutProps) {
+  const router = useRouter();
+  const drawerWidth = 250;
+
   return (
     <BaseLayout>
-      <div>
-        <Drawer variant="permanent" anchor="left">
+      <Box sx={{ display: "flex" }}>
+        <Drawer
+          variant="permanent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+          }}
+          PaperProps={{ elevation: 1 }}
+        >
+          <Toolbar>
+            <BrandLogo style={{ height: "1.5em" }} />
+          </Toolbar>
+          <Box
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "column",
+              gap: 3,
+              padding: 3,
+              width: drawerWidth,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                gap: 2,
+              }}
+            >
+              <TextField id="search" label="Search" size="small" />
+            </Box>
+            <Navigation />
+            <Projects />
+          </Box>
+        </Drawer>
+        <Drawer
+          variant="temporary"
+          anchor="left"
+          sx={{
+            display: { xs: "none", sm: "block" },
+          }}
+        >
           <Box
             sx={{
               alignItems: "center",
@@ -27,7 +73,7 @@ export default function NavLayout(props: NavLayoutProps) {
               flexDirection: "column",
               gap: 4,
               padding: 4,
-              width: 200,
+              width: drawerWidth,
             }}
           >
             <BrandLogo style={{ height: "1.5em" }} />
@@ -39,26 +85,36 @@ export default function NavLayout(props: NavLayoutProps) {
                 gap: 2,
               }}
             >
-              <ProjectControls />
+              <TextField id="search" label="Search" size="small" />
             </Box>
-            <List sx={{ backgroundColor: "", width: "100%" }}>
-              <ListItem>
-                <ListItemIcon />
-                <ListItemText>Overview</ListItemText>
-              </ListItem>
-              <ListItem>
-                <ListItemIcon />
-                <ListItemText>Issues</ListItemText>
-              </ListItem>
-              <ListItem>
-                <ListItemIcon />
-                <ListItemText>Boards</ListItemText>
-              </ListItem>
-            </List>
+            <Navigation />
+            <Projects />
           </Box>
         </Drawer>
-        <Box>Content</Box>
-      </div>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <AppBar
+            sx={{
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+            }}
+          >
+            <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography sx={{ textTransform: "capitalize" }}>
+                {router.asPath.slice(1)}
+              </Typography>
+              <CreateProject />
+            </Toolbar>
+          </AppBar>
+          <Box
+            sx={{
+              flexGrow: 1,
+              padding: 3,
+            }}
+          >
+            <Toolbar />
+            {props.children}
+          </Box>
+        </Box>
+      </Box>
     </BaseLayout>
   );
 }
