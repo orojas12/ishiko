e62c9d16a15249a2de998487be3d52e90fecf618
oscar <orojas12@outlook.com>
2024-01-11 00:22:49 -0700

    delete unused modules

--- client/src/lib/middleware/LoggingMiddleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { RequestMiddleware } from ".";
-
-export class LoggingMiddleware implements RequestMiddleware {
-    handleRequest = (request: NextRequest) => {
-        console.log("middleware logger: ", request.nextUrl.pathname);
-    };
-}
